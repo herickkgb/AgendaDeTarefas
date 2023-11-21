40ec57068e0aeb0469db83e5f6b972006f1db88f
Herick Pereira <42222113+herickkgb@users.noreply.github.com>
2023-11-20 23:19:39 -0300

    att contato

--- app/src/main/java/com/herick/agendadetarefas/adapter/ContatoAdapter.kt
+++ app/src/main/java/com/herick/agendadetarefas/adapter/ContatoAdapter.kt
@@ -1,17 +1,15 @@
 package com.herick.agendadetarefas.adapter
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.text.Html
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.herick.agendadetarefas.AppDataBase
 import com.herick.agendadetarefas.AtualizarUsuario
 import com.herick.agendadetarefas.dao.UsuarioDAO
-import com.herick.agendadetarefas.databinding.ActivityCadastrarUsuarioBinding
 import com.herick.agendadetarefas.databinding.ContatoItemBinding
 import com.herick.agendadetarefas.model.Usuario
 import kotlinx.coroutines.CoroutineScope
@@ -22,75 +20,68 @@ import kotlinx.coroutines.withContext
 class ContatoAdapter(private val context: Context, private val listaUsuario: MutableList<Usuario>) :
     RecyclerView.Adapter<ContatoAdapter.ContatoViewHolder>() {
 
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContatoViewHolder {
-        val itemLista = ContatoItemBinding.inflate(LayoutInflater.from(context), parent, false)
-        return ContatoViewHolder(itemLista)
+        val inflater = LayoutInflater.from(context)
+        val itemBinding = ContatoItemBinding.inflate(inflater, parent, false)
+        return ContatoViewHolder(itemBinding)
     }
 
     override fun getItemCount() = listaUsuario.size
 
     override fun onBindViewHolder(holder: ContatoViewHolder, position: Int) {
+        val usuario = listaUsuario[position]
 
-        holder.txtNome.text = "Nome: <i>${listaUsuario[position].nome}</i>"
-        holder.txtSobrenome.text = "<i>${listaUsuario[position].sobrenome}</i>"
-        holder.txtNumero.text = "Celular: ${listaUsuario[position].celular}"
+        with(holder) {
+            txtNome.text = "Nome: <i>${usuario.nome}</i>"
+            txtSobrenome.text = "<i>${usuario.sobrenome}</i>"
+            txtNumero.text = "Celular: ${usuario.celular}"
+            txtIdade.text = "Idade: <i>${usuario.idade}</i>"
 
-        holder.txtidade.text = "Idade: <i>${listaUsuario[position].idade}</i>"
+            // Permitindo que o TextView exiba formatação HTML
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                txtNome.text = Html.fromHtml(txtNome.text.toString(), Html.FROM_HTML_MODE_LEGACY)
+                txtSobrenome.text =
+                    Html.fromHtml(txtSobrenome.text.toString(), Html.FROM_HTML_MODE_LEGACY)
+                txtIdade.text = Html.fromHtml(txtIdade.text.toString(), Html.FROM_HTML_MODE_LEGACY)
+            } else {
+                @Suppress("DEPRECATION")
+                txtNome.text = Html.fromHtml(txtNome.text.toString())
+                txtSobrenome.text = Html.fromHtml(txtSobrenome.text.toString())
+                txtIdade.text = Html.fromHtml(txtIdade.text.toString())
+            }
 
-        // Permitindo que o TextView exiba formatação HTML
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
-            holder.txtNome.text =
-                Html.fromHtml(holder.txtNome.text.toString(), Html.FROM_HTML_MODE_LEGACY)
-            holder.txtSobrenome.text =
-                Html.fromHtml(holder.txtSobrenome.text.toString(), Html.FROM_HTML_MODE_LEGACY)
-            holder.txtidade.text =
-                Html.fromHtml(holder.txtidade.text.toString(), Html.FROM_HTML_MODE_LEGACY)
-        } else {
-            @Suppress("DEPRECATION")
-            holder.txtNome.text = Html.fromHtml(holder.txtNome.text.toString())
-            holder.txtSobrenome.text = Html.fromHtml(holder.txtSobrenome.text.toString())
-            holder.txtidade.text = Html.fromHtml(holder.txtidade.text.toString())
-        }
+            btAtualizar.setOnClickListener {
+                val intent = Intent(context, AtualizarUsuario::class.java).apply {
+                    putExtra("nome", usuario.nome)
+                    putExtra("sobrenome", usuario.sobrenome)
+                    putExtra("numero", usuario.celular)
+                    putExtra("idade", usuario.idade)
+                    putExtra("uid", usuario.uid)
+                }
+                context.startActivity(intent)
+            }
 
-        holder.btAtualizar.setOnClickListener {
-            val intent = Intent(context, AtualizarUsuario::class.java)
-
-            intent.putExtra("nome", listaUsuario[position].nome)
-            intent.putExtra("sobrenome", listaUsuario[position].sobrenome)
-            intent.putExtra("numero", listaUsuario[position].celular)
-            intent.putExtra("idade", listaUsuario[position].idade)
-            intent.putExtra("uid", listaUsuario[position].uid)
-
-            context.startActivity(intent)
-        }
-
-        holder.btDeletar.setOnClickListener {
-            CoroutineScope(Dispatchers.IO).launch {
-                val usuario = listaUsuario[position]
-                val usuarioDAO: UsuarioDAO = AppDataBase.getInstance(context).usuarioDao()
-                usuarioDAO.delete(usuario.uid)
-                listaUsuario.remove(usuario)
-
-                withContext(Dispatchers.Main) {
-                    notifyDataSetChanged()
+            btDeletar.setOnClickListener {
+                CoroutineScope(Dispatchers.IO).launch {
+                    val usuarioDAO: UsuarioDAO = AppDataBase.getInstance(context).usuarioDao()
+                    usuarioDAO.delete(usuario.uid)
+                    withContext(Dispatchers.Main) {
+                        listaUsuario.removeAt(position)
+                        notifyItemRemoved(position)
+                    }
                 }
             }
         }
-
     }
 
-    inner class ContatoViewHolder(binding: ContatoItemBinding) :
+    inner class ContatoViewHolder(private val binding: ContatoItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
         val txtNome = binding.textNome
         val txtSobrenome = binding.txtSobrenome
         val txtNumero = binding.textNumero
-        val txtidade = binding.textIdade
+        val txtIdade = binding.textIdade
         val btAtualizar = binding.imgEdit
         val btDeletar = binding.imgDelete
-
     }
-
-
 }
